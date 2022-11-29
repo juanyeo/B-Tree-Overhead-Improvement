62841116dcc049b42808fae08d9d481b1615306b
juanyeo <juanyeo@naver.com>
2022-11-29 23:18:21 +0900

    B+ Tree - idea 1, 2: Index-Based Merge Pass (최종 결과물)

---
+++ disk_bpt_improved/src/main.c
@@ -0,0 +1,56 @@
+#include "bpt.h"
+
+int main(){
+    int64_t input;
+    char instruction;
+    char buf[120];
+    char *result;
+    open_table("test.db");
+    while(scanf("%c", &instruction) != EOF){
+        switch(instruction){
+            case 'i':
+                scanf("%ld %s", &input, buf);
+                db_insert(input, buf);
+                break;
+            case 'f':
+                scanf("%ld", &input);
+                result = db_find(input);
+                if (result) {
+                    printf("Key: %ld, Value: %s\n", input, result);
+                }
+                else
+                    printf("Not Exists\n");
+
+                fflush(stdout);
+                break;
+            case 'd':
+                scanf("%ld", &input);
+                db_delete(input);
+                break;
+            case 'q':
+                while (getchar() != (int)'\n');
+                return EXIT_SUCCESS;
+                break;
+            case 'p':
+                db_print();
+                break;
+            case 't':
+                scanf("%ld", &input);
+                db_test(input);
+                break;
+            case 'h':
+                scanf("%ld", &input);
+                find_leaf_path(input);
+
+                fflush(stdout);
+                break;
+
+        }
+        while (getchar() != (int)'\n');
+    }
+    printf("\n");
+    return 0;
+}
+
+
+
